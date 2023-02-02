ff03a139dd6e5e0ab8c73add5d45e5b6a1587d9e
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-02-02 19:10:38 +0100

    STUFF GET PUT DELETE

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const Thing = require("./models/thing");
+
+const stuffRoutes = require("./routes/stuff");
+
+const app = express();
+
+
+
+mongoose
+  .connect(
+    "mongodb+srv://khnkaren:13131313Kk@cluster0.cm34v0j.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/stuff", stuffRoutes);
+
+
+module.exports = app;
+
+/**mongodb+srv://khnkaren:13131313Kk@cluster0.cm34v0j.mongodb.net/?retryWrites=true&w=majority */
