ac22696f700283640ed72a4a7147a31267ba7d36
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-02-09 18:22:30 +0100

    JWT AND MULTER

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,8 @@
 const bcrypt = require ('bcrypt');
 
-const User = require ('../models:/user');
+const User = require('../models:/user');
+
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
   bcrypt
@@ -37,7 +39,10 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: "TOKEN",
+            token: jwt.sign(
+              { userId: user._id }, "RANDOM_TOKEN_SECRET",
+              { expiresIn: "24h" }
+            )
           });
         })
         .catch((error) => res.status(500).json({ error }));
