0361b420096611db26c91800b8df8b56e798c93a
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-02-12 01:26:16 +0100

    multer in post stuff routes

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const auth = require("../middleware/auth");
 
+const multer = require("../middleware/multer-config");
+
 const stuffCtrl = require("../controllers/stuff");
 
 router.get("/", auth, stuffCtrl.getAllStuff);
-router.post("/", auth, stuffCtrl.createThing);
+router.post("/", auth, multer, stuffCtrl.createThing);
 router.get("/:id", auth, stuffCtrl.getOneThing);
 router.put("/:id", auth, stuffCtrl.modifyThing);
 router.delete("/:id", auth, stuffCtrl.deleteThing);
