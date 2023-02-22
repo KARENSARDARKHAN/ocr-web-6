73b6cb921a6390be8c823488fe447de8c4364fc2
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-02-22 22:54:47 +0100

    server adapt

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const app = require("./app");
+const server = http.createServer(app);
+
 
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
@@ -36,7 +38,6 @@ const errorHandler = (error) => {
   }
 };
 
-const server = http.createServer(app);
 
 server.on("error", errorHandler);
 server.on("listening", () => {
